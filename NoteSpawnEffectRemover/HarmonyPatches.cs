03d8c6e4de865153cacc26d69bdf42b1520616ab
Shadnix <shadnix@arcor.de>
2019-05-25 00:09:30 +0200

    Added comments

--- NoteSpawnEffectRemover/HarmonyPatches.cs
+++ NoteSpawnEffectRemover/HarmonyPatches.cs
@@ -12,6 +12,8 @@ namespace NoteSpawnEffectRemover.HarmonyPatches
     {
         static void Prefix(Color color, ref float animationDuration, Quaternion rotation)
         {
+            // Just skipping the init function also works, but will leave an unused BeatEffect object for every note spawn created by BeatEffectSpawner
+            // So instead, just shorten the animation duration from one second to 0 (effectively alive for one tick)
             animationDuration = 0f;
             return;
         }
